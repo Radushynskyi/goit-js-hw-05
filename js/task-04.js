bc7fa2f7b02f181c1a4dad792eb89118c220681a
Petro Radushynskyi <lsfreeman@ukr.net>
2020-07-27 17:56:05 +0300

    Added module-05

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,32 @@
 console.log("Задание 4");
 
+class StringBuilder {
+    constructor(str) {
+        this._value = str;
+    }
+    get value() {
+        return this._value;
+    }
+    append(str) {
+        this._value += str; 
+    }
+    prepend(str) {
+        this._value = str + this._value;
+    }
+    pad(str) {
+        this._value = str + this._value + str;
+    }
+}
 
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
 
 console.log("--------------");
--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,75 @@
 console.log("Задание 5");
 
-
+class Car {
+    static getSpecs(car) {
+        const {_maxSpeed, _speed, _isOn, _distance, _price} = car;
+        console.log(`maxSpeed: ${_maxSpeed}, speed: ${_speed}, isOn: ${_isOn}, distance: ${_distance}, price: ${_price}`);
+    }
+    
+    constructor({maxSpeed, price}) {
+        this._maxSpeed = maxSpeed;
+        this._speed = 0;
+        this._isOn = false;
+        this._distance = 0;
+        this._price = price;
+    }
+  
+    
+    get price() {
+        return this._price;
+    }
+    set price(value) {
+        this._price = value;
+    }
+    
+    turnOn() {
+        this._isOn = true;
+    }
+  
+    turnOff() {
+        this._isOn = false;
+        this._speed = 0;
+    }
+  
+    accelerate(value) {
+        if(this._speed < this._maxSpeed) {
+            this._speed += value;
+        }
+    }
+  
+    decelerate(value) {
+        if (this._speed - value > 0) {
+            this._speed -= value;
+        } else {
+            this._speed = 0;
+        }
+    }
+  
+    drive(hours) {
+        if(this._isOn) {
+            this._distance += hours * this._speed;
+        }
+    }
+  }
+  
+  const mustang = new Car({ maxSpeed: 200, price: 2000 });
+  
+  mustang.turnOn();
+  mustang.accelerate(50);
+  mustang.drive(2);
+  
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+  
+  mustang.decelerate(20);
+  mustang.drive(1);
+  mustang.turnOff();
+  
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+  
+  console.log(mustang.price); // 2000
+  mustang.price = 4000;
+  console.log(mustang.price); // 4000
 
 console.log("--------------");
